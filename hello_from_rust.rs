52f81ead78b16f27b14f69994f4edcec6ed03da6
Dmitri <272718+DimiG@users.noreply.github.com>
2019-03-24 17:42:59 +0300

    Added the hello_from_rust.rs file

---
+++ hello_from_rust.rs
@@ -0,0 +1,12 @@
+//! ========================================================================
+//! File: hello_from_rust.rs
+//! Creator: Dmitri G.
+//! Date: 2019-03-22
+//! Version: 0.0.1
+//! Description: "Hello from Rust" just a most simple Rust example!
+//! ========================================================================
+
+/// Main Routine
+fn main() {
+    println!("Hello from Rust!!!");
+}
